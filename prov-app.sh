3686242041abb67af42e8bcc9a9e6d35aa9f450c
tjmad43 <114770227+tjmad43@users.noreply.github.com>
2025-08-06 09:06:12 +0100

    Update prov-app.sh

--- prov-app.sh
+++ prov-app.sh
@@ -1,38 +1,77 @@
 #!/bin/bash
 # provision for sparta app
+# for Ubuntu 22.04
 
 echo "Update..."
-sudo DEBIAN_FRONTEND=noninteractiv apt update
+sudo DEBIAN_FRONTEND=noninteractive apt update
 echo "Done"
 echo
 
-# fix! expects user input
+echo "Upgrade..."
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+echo "Done"
+echo
 
-# fix! expects user input
-sudo apt-get install nginx -y
+echo "Install nginx..."
+sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx -y
+echo "Done"
+echo
 
 # configure reverse proxy using nginx
+echo "Configure nginx..."
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bk
+sudo sed -i 's|try_files \$uri \$uri/ =404;|proxy_pass http://localhost:3000;|' /etc/nginx/sites-available/default
+sudo systemctl reload nginx
+echo "Done"
+echo
 
 # install node.js v20 (installs npm commands too)
+echo "Install node.js"
 sudo DEBIAN_FRONTEND=noninteractive bash -c "curl -fsSL https://deb.nodesource.com/setup_20.x | bash -" && \
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
- 
- # check node version
+echo "Done"
+echo 
+
+# check node version
+echo "Check node version"
 node -v
+echo
 
 # download app code to a "repo" folder
+echo "Clone git repo"
 git clone https://github.com/tjmad43/tech508-sparta-app repo
+echo "Done"
+echo
 
 # cd into app folder
 cd repo/app
 
 # database instance private IP
-export DB_HOST=mongodb://172.31.26.123:27017/posts
+echo "Set db IP..."
+export DB_HOST=mongodb://172.31.26.66:27017/posts
+echo "Done"
+echo
+
+# seed database
+echo "Seed database..."
+node seeds/seed.js
+echo "Done"
+echo
 
 # install packages for app
+echo "Install packages..."
 npm install
+echo "Done"
+echo
+
+# install pm2
+echo "Install pm2"
+sudo npm install -g pm2
+echo "Done"
+echo
 
 # start app
-npm start
+echo "Start app..."
+pm2 start app.js
+pm2 save
 
